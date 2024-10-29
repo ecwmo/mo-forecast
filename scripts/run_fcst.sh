e58d638a04bf3bdb905d7aa07160891076e8d3ed
Kevin <khenson@observatory.ph>
2024-10-29 11:10:15 +0800

    ecmwf_feature added: download ecmwf bashscript and converting netcdf file to interfile python script
    modified: run_wps, run_wrf, and run_fcst

--- scripts/run_fcst.sh
+++ scripts/run_fcst.sh
@@ -70,9 +70,11 @@ mkdir -p "$MODEL_LOG_DIR"
 mkdir -p "$POST_LOG_DIR"
 rm -f "$ERROR_FILE"
 
+# Download WRF inputs
 if [ $DOWNLOAD_INPUT -eq 1 ]; then
   # download GFS
   source "$SCRIPT_DIR/download_gfs.sh"
+  source "$SCRIPT_DIR/download_ecmwf.sh"
 fi
 
 if [[ $WRF_MODE == '3dvar' && $DOWNLOAD_MADIS -eq 1 ]]; then
@@ -94,6 +96,27 @@ fi
 GFS_FILES=("$GFS_DIR"/*.grb)
 if [ ${#GFS_FILES[@]} -ne $NUM_TIMESTEPS ]; then
   err_msg="number of GFS Files: ${#GFS_FILES[@]}, expected: $NUM_TIMESTEPS"
+  echo "$err_msg" >"$ERROR_FILE"
+  source "$SCRIPT_DIR/send_alert.sh"
+  echo "$err_msg"
+  exit 1
+fi
+
+
+NUM_TIMESTEPS=$((WRF_FCST_DAYS * 24 + 1))
+ECMWF_FILES=("$ECMWF_DIR"/*.grib2)
+
+# Redownload ECMWF files if incomplete
+if [ ${#ECMWF_FILES[@]} -ne $NUM_TIMESTEPS ]; then 
+  echo "ECMWF files incomplete, redownloading.."
+  sleep 5m # allowance in case of internet disconnection/server overload
+  source "$SCRIPT_DIR/download_ecmwf.sh"
+fi
+
+# Send notifier if ECMWF files still incomplete
+ECMWF_FILES=("$ECMWF_DIR"/*.nc)
+if [ ${#ECMWF_FILES[@]} -ne $NUM_TIMESTEPS ]; then
+  err_msg="number of ECMWF Files: ${#ECMWF_FILES[@]}, expected: $NUM_TIMESTEPS"
   echo "$err_msg" >"$ERROR_FILE"
   source "$SCRIPT_DIR/send_alert.sh"
   echo "$err_msg"
@@ -192,7 +215,7 @@ prev_jid=$(sbatch "${slurm_opts[@]}")
 date=$(date +"%d")
 hostname=$(hostname -s)
 ### check date if 1st day of the month
-if [ $date == 01 ] && [ $hostname == "arowana" ]; then
+if [ "$date" == 01 ] && [ "$hostname" == "arowana" ]; then
     echo "Calculating Anomalies"  ### Anomaly plots
 
     mkdir -p "$POST_LOG_DIR/anomaly"
--- scripts/run_wps.sh
+++ scripts/run_wps.sh
@@ -4,32 +4,26 @@ source "$SCRIPT_DIR/set_env_wrf.run.sh"
 
 cd "${WRF_MAINDIR}/WPS" || exit
 
-mkdir "$NAMELIST_SUFF"
+mkdir -p "$NAMELIST_SUFF/gfs"
+
+WPS_NAMELIST_GFS="namelist.wps_gfs_$NAMELIST_SUFF"
 
 WPS_START_DATE="${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}:00:00"
 WPS_END_DATE="${FCST_YY2}-${FCST_MM2}-${FCST_DD2}_${FCST_ZZ2}:00:00"
-sed -i "4s/.*/\ start_date = '${WPS_START_DATE}','${WPS_START_DATE}',/" "namelist.wps_$NAMELIST_SUFF"
-sed -i "5s/.*/\ end_date   = '${WPS_END_DATE}','${WPS_END_DATE}',/" "namelist.wps_$NAMELIST_SUFF"
+sed -i "4s/.*/\ start_date = '${WPS_START_DATE}','${WPS_START_DATE}',/" "$WPS_NAMELIST_GFS"
+sed -i "5s/.*/\ end_date   = '${WPS_END_DATE}','${WPS_END_DATE}',/" "$WPS_NAMELIST_GFS"
 
-rm -f namelist.wps
-ln -s "namelist.wps_$NAMELIST_SUFF" namelist.wps
+ln -sf "$WPS_NAMELIST_GFS" namelist.wps
 
 rm -f "$NAMELIST_SUFF"/geo_em*
-echo "************************"
-echo "*   start of geogrid   *"
-echo "************************"
+echo "------------------"
+echo " Start of Geogrid "
+echo "------------------"
 srun -n 1 ./geogrid.exe >&log.geogrid &
 tail --pid=$! -f log.geogrid
-if ! tail -n 5 "log.geogrid" | grep -q "Successful"; then
-  echo "geogrid" >>"$ERROR_FILE"
-  echo "************************"
-  echo "*     geogrid error    *"
-  echo "************************"
-  exit 1
-fi
-echo "************************"
-echo "*    end of geogrid    *"
-echo "************************"
+echo "------------------"
+echo " End of Geogrid "
+echo "------------------"
 
 rm -f GRIBFILE.*
 # Update GFS link
@@ -38,40 +32,63 @@ ln -sf "$GFS_DIR"/*.grb "$WPS_GFSDIR"/.
 ./link_grib.csh "$WPS_GFSDIR"/*.grb
 #ln -s ungrib/Variable_Tables/Vtable.GFS Vtable #new line added; one time use
 
-rm -f FILE:*
-echo "************************"
-echo "*   start of ungrib    *"
-echo "************************"
+rm -f GFS:* 
+echo "------------------"
+echo " Start of Ungrib "
+echo "------------------"
 srun -n 1 ./ungrib.exe >&log.ungrib &
 tail --pid=$! -f log.ungrib
-if ! tail -n 5 "log.ungrib" | grep -q "Successful"; then
-  echo "ungrib" >>"$ERROR_FILE"
-  echo "************************"
-  echo "*     ungrib error     *"
-  echo "************************"
-  exit 1
-fi
-echo "************************"
-echo "*    end of ungrib     *"
-echo "************************"
+echo "------------------"
+echo " End of Ungrib "
+echo "------------------"
 
-rm -f "$NAMELIST_SUFF"/met_em.d0*
-echo "************************"
-echo "*   start of metgrid   *"
-echo "************************"
+rm -f "$NAMELIST_SUFF"/gfs/met_em.d0*
+echo " Start of Metgrid "
+echo "------------------"
 srun ./metgrid.exe >&log.metgrid &
-tail --pid=$! -f log.metgrid
-if ! tail -n 5 "log.metgrid" | grep -q "Successful"; then
-  echo "metgrid" >>"$ERROR_FILE"
-  echo "************************"
-  echo "*     metgrid error    *"
-  echo "************************"
-  exit 1
-fi
-echo "************************"
-echo "*    end of metgrid    *"
-echo "************************"
+tail --pid $! -f log.metgrid
+echo "------------------"
+echo " End of Metgrid "
+echo "------------------"
 
+
+mkdir -p "$NAMELIST_SUFF/ecmwf"
+
+WPS_NAMELIST_ECMWF="namelist.wps_ecmwf_$NAMELIST_SUFF"
+sed -i "4s/.*/\ start_date = '${WPS_START_DATE}','${WPS_START_DATE}',/" "$WPS_NAMELIST_ECMWF"
+sed -i "5s/.*/\ end_date   = '${WPS_END_DATE}','${WPS_END_DATE}',/" "$WPS_NAMELIST_ECMWF"
+
+ln -sf "$WPS_NAMELIST_ECMWF" namelist.wps
+
+
+# Update ECMWF link
+rm -f "$WPS_ECMWFDIR"/*.nc
+ln -sf "$ECMWF_NC_DIR"/*.nc "$WPS_ECMWFDIR"/.
+
+rm -f ECMWF:*
+echo "------------------"
+echo " Start of Ungrib "
+echo "------------------"
+cd "$SCRIPT_DIR/python" || exit
+$PYTHON convert_nc_interfile.py -i "$WPS_ECMWFDIR" -o "${WRF_MAINDIR}/WPS" "${FCST_YYYYMMDD}"
+cd "${WRF_MAINDIR}/WPS" || exit
+echo "------------------"
+echo " End of Ungrib "
+echo "------------------"
+
+rm -f "$NAMELIST_SUFF"/ecmwf/met_em.d0*
+echo " Start of Metgrid "
+echo "------------------"
+srun ./metgrid.exe >&log.metgrid &
+tail --pid $! -f log.metgrid
+echo "------------------"
+echo " End of Metgrid "
+echo "------------------"
+
+
+# cleanup
 rm -f geo_em*
-rm -f FILE:*
+rm -f GFS:*
+rm -f ECMWF:*
 rm -f GRIB*
+
