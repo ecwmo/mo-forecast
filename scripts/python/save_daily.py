cedb20f80bdb214e8d14552ec105a3e2ee908908
enricotana <etana@observatory.ph>
2025-04-10 17:01:07 +0800

    New Revisions + pre-commit modifications

--- scripts/python/save_daily.py
+++ scripts/python/save_daily.py
@@ -11,25 +11,33 @@ from config import Config
 
 conf = Config()
 resources_dir = conf.script_dir / "python/resources/nc"
-nc_dir = conf.data_dir / "daily/nc"
 inp_dir = os.getenv("WRF_SERVER_DIR")
 out_dir = conf.data_dir / "daily"
 
 
 def get_nrt(ds):
-    ds
     return ds.isel(time=slice(6))[["rain", "temp"]].mean("ens")
 
 
 def save_daily(day_offset=1):
     # Input: (day_offset) - number of days relative to day of processing which is always Yesterday (n=1). Change for earlier days.
     start = time.time()
+
+    # Create Directories
+    out_dir.mkdir(parents=True, exist_ok=True)
+    txt_dir = out_dir / "txt"
+    nc_dir = out_dir / "nc"
+
+    txt_dir.mkdir(parents=True, exist_ok=True)
+    nc_dir.mkdir(parents=True, exist_ok=True)
+
     # Get yesterday"s dataset from Dugong and Arowana output nc
     _day = datetime.today().date() - timedelta(days=day_offset)
-    print(f"Processing output from {_day}...")
+    _day_str = f"{_day:%Y-%m-%d}"
+    print(f"Processing output from {_day_str}...")
     try:
         _arowana = xr.open_mfdataset(
-            f"{inp_dir}/output.arowana/nc/wrf_{_day}_??.nc",
+            f"{inp_dir}/output.arowana/nc/wrf_{_day_str}_??.nc",
             concat_dim="time",
             combine="nested",
             preprocess=get_nrt,
@@ -38,7 +46,7 @@ def save_daily(day_offset=1):
         )
 
         _dugong = xr.open_mfdataset(
-            f"{inp_dir}/output.dugong/nc/wrf_{_day}_??.nc",
+            f"{inp_dir}/output.dugong/nc/wrf_{_day_str}_??.nc",
             concat_dim="time",
             combine="nested",
             preprocess=get_nrt,
@@ -47,7 +55,7 @@ def save_daily(day_offset=1):
         )
         _combined = _arowana.combine_first(_dugong)
     except OSError:
-        print(f'Files don"t exist for {_day}')
+        print(f'Files don"t exist for {_day_str}')
         end = time.time()
         elapsed = end - start
 
@@ -61,38 +69,37 @@ def save_daily(day_offset=1):
     _hours = _combined.time.dt.hour.values
     _missing = [item for item in _complete if item not in _hours]
     if len(_missing) == 0:  # For complete data
-        print(f"Dataset is complete\nSaving Dataset for {_day}...")
+        print(f"Dataset is complete\nSaving Dataset for {_day_str}...")
         _final = _combined.resample({"time": "D"}).mean()
         _final["rain"] = (
             _combined["rain"].resample({"time": "D"}).sum()
         )  # Since rain should be accumulated and not mean
-        _final.load().to_netcdf(out_dir / f"nc/wrf_{_day}.nc")
+        _final.load().to_netcdf(out_dir / f"nc/wrf_{_day_str}.nc")
 
     # For incomplete and partially complete Datasets
-    if len(_missing) > 4:  # For incomplete data
-        _ref_dates = pd.date_range(start=f"{_day}", periods=24, freq="H")
+    elif len(_missing) > 4:  # For incomplete data
+        _ref_dates = pd.date_range(start=f"{_day_str}", periods=24, freq="H")
         _act_dates = pd.Index(_combined.time.values)
         _missing_hours = _ref_dates.difference(_act_dates)
-        _missing_hours.to_series().to_csv(out_dir + f"txt/Missing_{_day}.txt")
+        _missing_hours.to_series().to_csv(out_dir + f"txt/Missing_{_day_str}.txt")
         print(f"Missing Hours: {len(_missing)}\nNot Saving Dataset...")
 
-    if (
-        len(_missing) < 4 and len(_missing) > 0
-    ):  # For partially complete data (<4 hrs missing)
-        print(f"Missing Hours: {len(_missing)}\nSaving Dataset for {_day}...")
+    # len(_missing) < 4 and len(_missing) > 0
+    else:  # For partially complete data (<4 hrs missing)
+        print(f"Missing Hours: {len(_missing)}\nSaving Dataset for {_day_str}...")
         # Logging of missing data
-        _ref_dates = pd.date_range(start=f"{_day}", periods=24, freq="H")
+        _ref_dates = pd.date_range(start=f"{_day_str}", periods=24, freq="H")
         _act_dates = pd.Index(_combined.time.values)
         _missing_hours = _ref_dates.difference(_act_dates)
         _missing_hours.to_series().to_csv(
-            out_dir + f"txt/P_Missing_{_day}.txt"
+            out_dir + f"txt/P_Missing_{_day_str}.txt"
         )  # Still logs partially missing days
         # Process and Save Dataset
         _final = _combined.resample({"time": "D"}).mean()
         _final["rain"] = (
             _combined["rain"].resample({"time": "D"}).sum()
         )  # Since rain should be accumulated and not mean
-        _final.load().to_netcdf(out_dir / f"nc/wrf_{_day}.nc")
+        _final.load().to_netcdf(out_dir / f"nc/wrf_{_day_str}.nc")
 
     end = time.time()
     elapsed = end - start
@@ -102,13 +109,4 @@ def save_daily(day_offset=1):
 
 
 if __name__ == "__main__":
-    out_dir.mkdir(parents=True, exist_ok=True)
-    img_dir = out_dir / "img"
-    txt_dir = out_dir / "txt"
-    nc_dir = out_dir / "nc"
-
-    img_dir.mkdir(parents=True, exist_ok=True)
-    txt_dir.mkdir(parents=True, exist_ok=True)
-    nc_dir.mkdir(parents=True, exist_ok=True)
-
     save_daily()
