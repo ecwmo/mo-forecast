23fadfb98af80350d426d91169e7a1e2d8e39fb0
Kyle <ebadua@observatory.ph>
2026-01-20 18:14:17 +0800

    run plotting weather chart after downloading gfs

--- scripts/download_gfs.sh
+++ scripts/download_gfs.sh
@@ -77,8 +77,23 @@ cd "$SCRIPT_DIR/python" || exit
 # Convert GFS precipitation grb files to .nc
 $PYTHON convert_gfs_nc.py -i "$GFS_DIR" -o "$GFS_NC_DIR"
 
-cd "$MAINDIR" || exit
-
 echo "---------------------------------"
 echo " Finished downloading GFS files! "
 echo "---------------------------------"
+
+# Extract GFS variables needed for plotting
+$PYTHON extract_gfs_surfacevar.py -i "$GFS_DIR" -o "$GFS_NC_DIR"
+
+echo "---------------------------------"
+echo " Start plotting weather charts!  "
+echo "---------------------------------"
+
+# Plot surface weather charts with shearline
+GFS_OUT_FILE="${GFS_NC_DIR}/gfs_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_3hrly.nc"
+$PYTHON plot_weatherchart.py -i "$GFS_OUT_FILE" -o "$OUTDIR"
+
+echo "---------------------------------"
+echo "Plotting weather charts finished!"
+echo "---------------------------------"
+
+cd "$MAINDIR" || exit
